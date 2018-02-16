2d2f8de904a9391834cdf69b715df991c297acc6
jagrelot <jagrelot@acumensolutions.com>
2018-02-16 16:43:56 -0500

    Update

--- Refresh.sh
+++ Refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ant retrieve
 pwd
-rsync -avzh --progress ./src / ../../../../../OrgDemoReop/RespositoryOne-Git/src
+rsync -avzh ./src / ../../../../../OrgDemoReop/RespositoryOne-Git/src
 cd src
 rm -rf documents cleanDataServices profiles/Chatter\ Only\ User.profile
