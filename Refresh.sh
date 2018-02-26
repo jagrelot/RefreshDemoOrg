220ec6aee9ae3d3b2b3b82a10dfd958f2cbd8a3e
jagrelot <jagrelot@acumensolutions.com>
2018-02-26 14:33:31 -0500

    Update

--- Refresh.sh
+++ Refresh.sh
@@ -2,7 +2,7 @@
 ant retrieve
 pwd
 # Move current dirctory(Jenkins Workspace) to org repository
-rsync -av --progress ./src ~/OrgDemoReop/RepositoryOne-Git/src --exclude package.xml 
+rsync -av --progress ./src /OrgDemoReop/RepositoryOne-Git/src --exclude package.xml 
 # Remove folders and files for successful buils
 # Update xml files for build fixes
 # Commit to Git 
