c56312b93dd23802fba118d6739845767bf0ce6f
jagrelot <jagrelot@acumensolutions.com>
2018-02-26 14:29:15 -0500

    Update

--- Refresh.sh
+++ Refresh.sh
@@ -2,8 +2,11 @@
 ant retrieve
 pwd
 # Move current dirctory(Jenkins Workspace) to org repository
-# mv ./src!(package.xml)  ~/OrgDemoReop/RepositoryOne-Git/src
-
+rsync -av --progress ./src ~/OrgDemoReop/RepositoryOne-Git/src --exclude package.xml 
+# Remove folders and files for successful buils
+# Update xml files for build fixes
+# Commit to Git 
+# Use another Jenkins Job to kick off build
 
 
 
